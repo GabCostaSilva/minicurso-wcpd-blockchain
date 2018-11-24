07022415651bf02cc6b0e095b5dab994a4c5ce78
Gabriel Costa <gabcostadeveloper@gmail.com>
2018-11-24 20:07:11 -0300

    Update DontCallMeUber.sol

--- DontCallMeUber.sol
+++ DontCallMeUber.sol
@@ -2,65 +2,80 @@ pragma solidity ^0.4.20;
 
 contract DontCallMeUber {
     
+      enum TripStatus {
+        WAITING,
+        ACCEPTED,
+        REFUSED,
+        STARTED,
+        FINALIZED
+    }
+    
+    
     struct Trip {
         uint value;
-        uint driverId;
+        address driverId;
+        TripStatus status;
     }
     
     struct Driver {
-        uint id;
+        address id;
         bool isFree;
     }
-    
-    uint numDrivers;
-    mapping(uint => Driver) drivers;
+
+    mapping(address => Driver) drivers;
     
     uint numTrips;
     mapping(uint => Trip) trips;
     
-    function NoUber() {
-        numDrivers = 0;
+    function DontCallMeUber() {
         numTrips = 0;
     }
-    
-    function signUp() {
-        Driver driver = drivers[++numDrivers];
-        driver.id = numDrivers;
+    function signUp(address _driver) {
+        Driver driver = drivers[_driver];
+        driver.id = msg.sender;
         driver.isFree = true;
     }
     
-    function signTrip(uint _driver, uint _value) {
+    function signTrip(address _driver, uint _value) {
         Trip trip = trips[++numTrips];
         Driver driver = drivers[_driver];
         trip.value= _value;
         trip.driverId = driver.id;
+        trip.status = TripStatus.WAITING;
     }
     
-    function callDriver(uint _driver) {
+    function callDriver(address _driver) {
         Driver driver = drivers[_driver];
         require( driver.isFree );
-         driver.isFree = false;
+        driver.isFree = false;
     }
     
-    function acceptTrip(uint _trip, uint _value, uint _driver) payable{
+    function acceptTrip(address _driver, uint _trip, uint _value) payable{
         Trip trip = trips[_trip];
         Driver driver = drivers[_driver];
-        
         trip.value = _value;
+        
+        require( trip.value <= msg.value );
+        require( msg.sender != driver.id ); 
+        
+        trip.status = TripStatus.ACCEPTED;
+        trip.driverId = msg.sender;
         driver.isFree = false;
+        
+        var payment = _value;
+        
+        driver.id.transfer(payment);
         
     }
     
-    // TODO
     function startTrip(uint _trip) {
         Trip trip = trips[_trip];
-        
+        trip.status = TripStatus.STARTED;
     }
     
     function endTrip(uint _trip) {
         Trip trip = trips[_trip];
+        trip.status = TripStatus.FINALIZED;
     }
-    
-    // CREATE ENUM FOR TRIP STATUS
-    
+
 }
